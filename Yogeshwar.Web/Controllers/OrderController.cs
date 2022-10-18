2f079deb9999d9a259d4f3e47e5263b90fa060a6
harshad <harshad@silverminegroup.com>
2022-10-19 01:32:19 +0530

    Add some menue in Layout file and Create design for order module list and addedit

---
+++ Yogeshwar.Web/Controllers/OrderController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Yogeshwar.Web.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AddEdit()
+        {
+            return View();
+        }
+
+        public IActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
