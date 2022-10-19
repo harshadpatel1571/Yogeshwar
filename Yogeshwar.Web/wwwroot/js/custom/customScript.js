4e83f7970ca7c5df37024f25bf58c8fdad670564
harshad <harshad@silverminegroup.com>
2022-10-20 00:02:21 +0530

    Change Order page design and create new design for 404 and 500

--- Yogeshwar.Web/wwwroot/js/custom/customScript.js
+++ Yogeshwar.Web/wwwroot/js/custom/customScript.js
@@ -197,3 +197,32 @@ $(function () {
         "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
     }).buttons().container().appendTo('#grid2_wrapper .col-md-6:eq(0)');
 });
+
+function isData() {
+    var t = document.getElementsByClassName("plus"),
+        e = document.getElementsByClassName("minus"),
+        n = document.getElementsByClassName("product");
+    t &&
+        Array.from(t).forEach(function (t) {
+            t.addEventListener("click", function (e) {
+                parseInt(t.previousElementSibling.value) < e.target.previousElementSibling.getAttribute("max") &&
+                    (e.target.previousElementSibling.value++,
+                        n &&
+                        Array.from(n).forEach(function (t) {
+                            updateQuantity(e.target);
+                        }));
+            });
+        }),
+        e &&
+        Array.from(e).forEach(function (t) {
+            t.addEventListener("click", function (e) {
+                parseInt(t.nextElementSibling.value) > e.target.nextElementSibling.getAttribute("min") &&
+                    (e.target.nextElementSibling.value--,
+                        n &&
+                        Array.from(n).forEach(function (t) {
+                            updateQuantity(e.target);
+                        }));
+            });
+        });
+}
+isData();
