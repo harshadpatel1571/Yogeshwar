c835d00b5f74cc07deecca5e953ca937911a4a65
harshadpatel1571 <harshadpatel1571@gmail.com>
2023-03-24 11:05:12 +0530

    harshad changes for ck editor

--- Yogeshwar.Web/wwwroot/js/custom/categoiesDatatable.js
+++ Yogeshwar.Web/wwwroot/js/custom/categoiesDatatable.js
@@ -2,10 +2,17 @@
     var columns = [
         { data: "id", name: "Id", "autoWidth": true },
         {
+            name: "image", "autoWidth": true,
+            sDefaultContent: "--",
+            render: function (data, type, row) {
+                return "<img src='https://localhost:44320/DataImages/Accessories/0fd2d2e151554ce09af62071c0ff5714.webp' height='50' width='50'/>";
+            }
+        },
+        {
             name: "name", "autoWidth": true,
             sDefaultContent: "--",
             render: function (data, type, row) {
-                return "<a href='/Categories/Detail/" + row.id + "' style='color:hotpink' >" + row.name + "</a>";
+                return "<p style='color:hotpink' >" + row.name + "</p>";
             }
         },
         {
@@ -33,64 +40,3 @@
     ];
     BindGrid("/Categories/BindData/", columns, [1], [1], [1]);
 });
-
-//$(document).ready(function () {
-//    const table = $('#grid').DataTable();
-//    $('.dataTables_filter input')
-//        .unbind()
-//        .bind('input', function (e) {
-//            if (this.value.length >= 3 || e.keyCode === 13) {
-//                table.search(this.value).draw();
-//            }
-//            if (this.value === "") {
-//                table.search("").draw();
-//            }
-//        });
-//});
-
-//function deleteRecord(id) {
-//    Swal.fire({
-//        title: "Are you sure?",
-//        text: "You won't be able to revert this!",
-//        icon: "warning",
-//        showCancelButton: !0,
-//        confirmButtonClass: "btn btn-primary w-xs me-2 mt-2",
-//        cancelButtonClass: "btn btn-danger w-xs mt-2",
-//        confirmButtonText: "Yes, delete it!",
-//        buttonsStyling: !1,
-//        showCloseButton: !0,
-//    }).then(function (t) {
-//        if (!t.isConfirmed) return;
-//        $.ajax({
-//            type: "POST",
-//            url: "/Categories/Delete/" + id,
-//            success: function () {
-//                t.value && Swal.fire({
-//                    title: "Deleted.",
-//                    text: "Your record has been deleted.",
-//                    icon: "success",
-//                    confirmButtonClass: "btn btn-primary w-xs mt-2",
-//                    buttonsStyling: !1
-//                }).then(function () {
-//                    const table = $("#grid").DataTable();
-//                    table.ajax.reload(null, false);
-//                });
-//            },
-//            error: function (response) {
-//                let message = "This entity is being referred somewhere else.";
-
-//                if (response.status === 404) {
-//                    message = "Entity can not be found.";
-//                }
-
-//                t.value && Swal.fire({
-//                    title: "Unable to delete.",
-//                    text: message,
-//                    icon: "warning",
-//                    confirmButtonClass: "btn btn-primary w-xs mt-2",
-//                    buttonsStyling: !1
-//                })
-//            }
-//        });
-//    });
-//}
